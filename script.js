7fd1b2047683a6c1c9c02cddee382718c5a5132b
dwirhm22 <dwi.123490046@student.itera.ac.id>
2025-03-12 09:30:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', function() {
+        alert('Lihat koleksi terbaru kami di Midnight Blush!');
+    });
+});
